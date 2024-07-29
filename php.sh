0536384184689e99d8e08c3822936baca636515b
Murilo Elias <muriloelias@ipeweb.com.br>
2024-07-29 09:56:49 -0300

    [update] - update

---
+++ php.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+PROGRAMS=(
+  php8.1 
+  php8.1-pgsql
+  php8.1-curl
+  php8.1-dev
+)
+
+sudo apt update -y
+
+for nome_do_programa in "${PROGRAMS[@]}"; do
+  if ! dpkg -l | grep -q "${nome_do_programa}"; then
+    apt install "$nome_do_programa" -y
+  else
+    echo "[Install] - $nome_do_programa"
+  fi
+done
---
+++ vscode.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo apt update
+sudo snap install code-insiders --classic

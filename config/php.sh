5987d7835036f3055cdf90db7b0914d8c0209d87
iloElias <murilo7456@gmail.com>
2024-10-20 21:21:34 -0300

    [update] - updated files

--- config/php.sh
+++ config/php.sh
@@ -2,17 +2,19 @@
 
 PROGRAMS=(
   php8.3
+  php8.3-common
   php8.3-pgsql
   php8.3-curl
   php8.3-dev
+  php8.3-pdo
 )
 
 sudo apt update -y
 
 for nome_do_programa in "${PROGRAMS[@]}"; do
   if ! dpkg -l | grep -q "${nome_do_programa}"; then
-    apt install "$nome_do_programa" -y
+    sudo apt install "$nome_do_programa" -y
   else
-    echo "[Install] - $nome_do_programa"
+    echo "[Install] - $nome_do_programa is already installed"
   fi
 done

181dad940c9ab898c1598605cabf9fe71abbad84
iloElias <murilo7456@gmail.com>
2025-10-29 18:25:44 -0300

    feat: add --force/-f handling to stop-all-containers.sh to kill target containers and exit

--- scripts/stop-all-containers.sh
+++ scripts/stop-all-containers.sh
@@ -2,11 +2,15 @@
 
 source /opt/dotfiles/lib/utils.sh
 
+force_stop=false
 ignore_excluded=false
 
 for arg in "$@"; do
   case $arg in
     --ignore-excluded|--ignore-exclude) ignore_excluded=true ;;
+  esac
+  case $arg in
+    --force|-f) force_stop=true ;;
   esac
 done
 
@@ -14,6 +18,10 @@ mapfile -t containers_to_stop < <(get_target_containers "$ignore_excluded")
 
 if [[ ${#containers_to_stop[@]} -gt 0 ]]; then
   echo "Parando contêineres: ${containers_to_stop[*]}"
+  if $force_stop; then
+    docker kill "${containers_to_stop[@]}"
+    exit 0
+  fi
   docker stop "${containers_to_stop[@]}"
 else
   echo "Nenhum contêiner a ser parado."

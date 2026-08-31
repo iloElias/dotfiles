d92b454bce0201e7a3a50b9bec6b7d3d5aaedb56
iloElias <murilo7456@gmail.com>
2026-08-30 22:35:32 -0300

    feat: backup config

---
+++ config/grub.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/vinceliuice/Elegant-grub2-themes.git
+
+cd Elegant-grub2-themes
+
+sudo ./install.sh -b -t forest -p window -i left -c dark -s 1080p
+
